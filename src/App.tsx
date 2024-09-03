3bf7df8c8fc0250f7867777cc6b8513541bcf8f8
VM <lersidi@gmai.com>
2024-09-02 20:27:15 -0500

    implement reducer fuctions

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import "./App.css";
 import TodoList from "./react-query/TodoList";
 import PostList from "./react-query/PostList";
 import TodoForm from "./react-query/TodoForm";
+import Counter from "./state-management/Counter";
+import TaskList from "./state-management/TaskList";
+import LoginStatus from "./state-management/LoginStatus";
 
 function App() {
   return (
     <>
-      <TodoForm />
-      <TodoList />
+      <LoginStatus />
     </>
   );
 }
